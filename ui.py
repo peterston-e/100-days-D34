f79a1d88cf8f171d1e707eacf2ca34743c6aae96
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-12-19 22:10:04 +0000

    pulled in question ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ("Ariel", 20, "italic")
@@ -8,7 +9,8 @@ score = 0
 
 class QuizzInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizz Game")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -20,7 +22,7 @@ class QuizzInterface:
         self.question_text = self.canvas.create_text(
             150, 125,
             text=placeholder, font=FONT,
-            fill=THEME_COLOR, width=300)
+            fill=THEME_COLOR, width=280)
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         tick_image = PhotoImage(file="images/true.png")
@@ -31,4 +33,10 @@ class QuizzInterface:
         self.false_button = Button(image=cross_image, highlightbackground=THEME_COLOR)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
